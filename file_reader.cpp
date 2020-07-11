5c08f0e3a2cd830dabf9d118bbe87b0a06076bc4
Kruza <macik1@hotmail.com>
2020-07-11 14:05:14 +0200

    Added extraction of neighbours, graph matrix and print function

--- file_reader.cpp
+++ file_reader.cpp
@@ -3,24 +3,29 @@
 #include <iostream>
 #include <cstring>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
 string file_path = ".\\02_sample_data\\10Neighbours_30PercentRed\\tree_10neigh_v0.txt";
 
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 bool does_file_exist(string user_file_path)
 {
 	string line;
 
-	/* User didn't specified new path */
-	if (user_file_path.length() != 0)
+	/* User used default */
+
+	if (user_file_path == "default")
 	{
-		file_path = user_file_path;
+		cout << "User didn't provide any path.\n";
+		cout << "Using default path: " << file_path << '\n';
 	}
 	else
 	{
-		cout << "User didn't provide any path.\n";
-		cout << "Using default path: " << user_file_path << '\n';
+		file_path = user_file_path;
 	}
 
 	ifstream input_file (file_path);
@@ -48,15 +53,124 @@ bool does_file_exist(string user_file_path)
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-int ** extract_graph_from_file(string file_path)
+bool extract_graph_from_file(int ** graph_matrix, int matrix_size, vector<int> &neighbours_v)
 {
 	string tmp;
 	string number;
-	int size_of_graph;
-	int ** p_graph;
-	int matrix_size;
 	size_t one_index;
 	int neighbour_index;
+
+	/* Opening file */
+	ifstream file_stream;
+	file_stream.open(file_path);
+
+	/* Skip two lines */
+	getline(file_stream, tmp);
+	getline(file_stream, tmp);
+
+	/* fill up matix array */
+	for (int i = 0; i < matrix_size; ++i)
+	{
+		memset(graph_matrix[i], 0, matrix_size*sizeof(int));
+
+		/* Set '1' to the right fields of matrix */
+		one_index = tmp.find_first_of('1');
+		while (one_index != std::string::npos)
+		{
+			graph_matrix[i][one_index/3] = 1;
+			one_index = tmp.find_first_of('1', one_index+1);
+		}
+		getline(file_stream, tmp);	
+	} 
+
+	/* Set "neighbours" of the matrix */
+	getline(file_stream, tmp);
+	for(int k = 0; k < tmp.length() - 1; ++k)
+	{
+		number += tmp[k];
+
+		if(tmp[k] == ',')
+		{
+			neighbour_index = stoi(number);
+			neighbours_v.push_back(neighbour_index);
+			number = "";
+		} 
+	}
+
+	cout << "Data extracted correctlly" << endl;
+	file_stream.close();
+
+	/* Assaing arrays to input arrays */
+	return true;
+} 
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int ** allocate_space_for_graph_matrix(int matrix_size)
+{
+	string tmp;
+	int ** p_graph;
+
+	/* Opening file */
+	ifstream file_stream;
+	file_stream.open(file_path);
+
+	/* Skip two lines */
+	getline(file_stream, tmp);
+	getline(file_stream, tmp);
+
+	/* Allocate adequete memory block for matrix and extract data*/
+	p_graph = new (nothrow) int * [matrix_size];
+	for (int i = 0; i < matrix_size; ++i)
+	{
+		p_graph[i] = new (nothrow) int[matrix_size];
+		getline(file_stream, tmp);	
+	} 
+
+	// Check if p_graph pointer is not NULL
+	if(p_graph == NULL) 
+	{
+		cout << "Memory allocation failed." << endl;
+		return p_graph;
+	}
+	cout << "Memory allocated correctlly." << endl;	
+
+	return p_graph;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void print_graph_matrix_and_neighbours(int ** in_graph, int rows, int columns, vector<int> &in_neighbours)
+{
+
+	/* Printing graph matrix */
+	cout << "Graph matrix:" << endl;
+	for (int i = 0; i < rows; ++i)
+	{
+		for (int j = 0; j < columns; ++j)
+		{
+			cout << in_graph[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+
+	/* Printing neighbours vector */
+	cout << "Neighbours:" << endl;
+	for (int k = 0; k < in_neighbours.size(); ++k)
+	{
+		cout << in_neighbours[k] << " ";
+	}
+
+	cout << endl;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int get_size_of_matrix()
+{
+	string tmp;
+	int matrix_size;
+	int size_of_graph;
 
 	/* Opening file */
 	ifstream file_stream;
@@ -72,56 +186,5 @@ int ** extract_graph_from_file(string file_path)
 	/* Set right size of matrix */
 	matrix_size = (size_of_graph - 1)/3;
 
-	/* Allocate adequete memory block for matrix and extract data*/
-	p_graph = new (nothrow) int * [matrix_size];
-	for (int i = 0; i < matrix_size; ++i)
-	{
-		p_graph[i] = new (nothrow) int[matrix_size];
-		memset(p_graph[i], 0, matrix_size*sizeof(int));
-
-		/* Set '1' to the right fields of matrix */
-		one_index = tmp.find_first_of('1');
-		while (one_index != std::string::npos)
-		{
-			p_graph[i][one_index/3] = 1;
-			one_index = tmp.find_first_of('1', one_index+1);
-		}
-		getline(file_stream, tmp);	
-	} 
-
-	// Check if p_graph pointer is not NULL
-	if(p_graph == NULL) 
-	{
-		cout << "Memory allocation failed." << endl;
-		return p_graph;
-	}
-	cout << "Memory allocated correctlly." << endl;
-
-	/* Set "neighbours" on the diagonal of the matrix */
-	getline(file_stream, tmp);
-	cout << tmp;
-
-	for(int k = 0; k < tmp.length() - 1; ++k)
-	{
-		number += tmp[k];
-
-		if(tmp[k + 1] == ',')
-		{
-			neighbour_index = stoi(number);
-			cout << neighbour_index << endl;
-			p_graph[neighbour_index][neighbour_index] = -2;
-			number = "";
-		} 
-		else if ((k + 1) == " ")
-		{
-			/* code */
-		}
-
-	}
-
-	cout << "Data extracted correctlly" << endl;
-	file_stream.close();
-	return p_graph;
-} 
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	return matrix_size;
+}
--- main.cpp
+++ main.cpp
@@ -5,23 +5,28 @@
 #include <fstream>
 #include "file_reader.cpp"
 
-#define DEBUG_MODE 1;
-
-string user_file_path;
-
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
+	int ** p;
+	int size = 0;
+	vector<int> v;
+	string user_file_path;
+
 	while(1)
 	{
 		cout << "Please provide path to your input file:" << endl;
+		cout << "(To proviade default file path please tpye 'default')" << endl;
 		cin >> user_file_path;
 		if (does_file_exist(user_file_path)) break;
 	}
 
-	int ** p;
-	p = extract_graph_from_file(user_file_path);
+	size = get_size_of_matrix();
+	p = allocate_space_for_graph_matrix(size);
+	extract_graph_from_file(p, size, v);
+	print_graph_matrix_and_neighbours(p, size, size, v);
+
 	delete[] p;
 	return 0;
 }
