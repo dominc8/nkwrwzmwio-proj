1f52c34ffed267f1b0dc98e09ac70d9288d9bae8
dominc8 <kormandominik@gmail.com>
2020-07-25 10:37:15 +0200

    Refactoring and modifying marking red neighbours in the matrix

--- file_reader.h
+++ file_reader.h
@@ -2,9 +2,11 @@
 
 struct square_graph
 {
-	uint8_t *nodes_matrix;
-	uint16_t size = 0;
+    uint8_t *nodes_matrix;
+    uint16_t size = 0;
 };
+
+bool read_data_from_file(square_graph *main_graph);
 
 bool does_file_exist(const std::string &user_file_path);
 
@@ -15,3 +17,4 @@ bool get_size_of_matrix(square_graph *graph);
 bool allocate_space_for_graph(square_graph *graph);
 
 void print_graph(square_graph *graph);
+
--- main.cpp
+++ main.cpp
@@ -5,58 +5,17 @@
 #include <fstream>
 #include "file_reader.h"
 
-using namespace std;
-
-bool read_data_from_file(struct square_graph *main_graph)
-{
-	string user_file_path;
-
-	/* Get filepath from user and check if it exists */
-	while(1)
-	{
-		std::cout << "Please provide path to your input file:" << endl;
-		std::cout << "(To provide default file path please type 'default')" << endl;
-
-		std::cin >> user_file_path;
-
-		if (does_file_exist(user_file_path)) 
-			break;
-	}
-
-	if (!get_size_of_matrix(main_graph))
-	{
-		cout << "Getting graph size failed!" << endl;
-		return false;
-	}
-
-	if (!allocate_space_for_graph(main_graph))
-	{
-		cout << "Failed to allocate memory for graph!" << endl;
-		return false;
-	}
-
-	if (!get_graph_from_file(main_graph))
-	{
-		cout << "Getting data form file failed!" << endl;
-		return false;
-	}
-
-	cout << "Graph read succesfully!" <<endl;
-
-	return true;
-}
-
-
 int main(int argc, char const *argv[])
 {
-	struct square_graph main_graph;
+    square_graph main_graph;
 
-	if (!read_data_from_file(&main_graph))
-		return 0;
+    if (!read_data_from_file(&main_graph))
+        return -1;
 
-	print_graph( &main_graph);
+    print_graph(&main_graph);
 
-	delete[] main_graph.nodes_matrix;
+    delete[] main_graph.nodes_matrix;
 
-	return 0;
+    return 0;
 }
+
